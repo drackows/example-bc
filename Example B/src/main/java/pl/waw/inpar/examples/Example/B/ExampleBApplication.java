81e776c3ae5b3f4000261eafff6196748fdf8f33
drackowski <drackows@gmail.com>
2018-09-18 16:49:29 +0200

    init

---
+++ Example B/src/main/java/pl/waw/inpar/examples/Example/B/ExampleBApplication.java
@@ -0,0 +1,12 @@
+package pl.waw.inpar.examples.Example.B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleBApplication.class, args);
+	}
+}
---
+++ Example C/src/main/java/pl/waw/inpar/examples/Example/C/ExampleCApplication.java
@@ -0,0 +1,12 @@
+package pl.waw.inpar.examples.Example.C;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleCApplication.class, args);
+	}
+}
